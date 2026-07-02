855a2798f88576ce066671c6dfc31afd4bd78c9b
André Simão <andresimao353@gmail.com>
2026-07-02 15:55:11 +0100

    feat: clean ui of the project

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main className="flex justify-center items-center flex-col gap-4 max-w-xl mx-auto py-8 px-1 text-justify">
+      <h1>🎬 Edita Criativo</h1>
+      <p>
+        Landing Page institucional desenvolvida para a Edita Criativo, um
+        estúdio especializado em edição profissional de vídeos para criadores de
+        conteúdo, empresas e marcas.
+      </p>
+      <p>
+        O objetivo deste projeto é apresentar os serviços da empresa, fortalecer
+        sua presença digital e converter visitantes em clientes através de uma
+        experiência moderna, elegante e altamente responsiva.
+      </p>
+    </main>
+  );
+}
